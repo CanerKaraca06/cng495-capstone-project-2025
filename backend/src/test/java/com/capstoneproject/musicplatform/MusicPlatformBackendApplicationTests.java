d4f7a8844e1d9319b3d75e17cc383182e5300f65
CanerKaraca06 <caner_krc@hotmail.com>
2025-11-07 01:21:19 +0300

    Moved the backend to the backend/ directory

---
+++ backend/src/test/java/com/capstoneproject/musicplatform/MusicPlatformBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capstoneproject.musicplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicPlatformBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
